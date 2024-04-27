7769e882cfbd3ef10c938fd1809c6691f51fe8b0
denzelbautista <denzel.bautista@utec.edu.pe>
2024-04-26 20:56:55 -0500

    a

--- 2024-1/linkd.cpp
+++ 2024-1/linkd.cpp
@@ -1,23 +1,16 @@
 #include <iostream>
 using namespace std;
 
-struct node {
-  int data;
-  node* next;
-  node() {}
-  node(int _data, node* _next = nullptr) {
-    data = _data;
-    next = _next;
-  }
-  // otra forma
-  // node(int _data, node* _next) : data(_data),next(_next){}
-};
-
-class fwdlist {
+template <typename T>
+class ForwardList {
  private:
-  node* head;
-
- public:
+  struct Node {
+   private:
+    T data;
+    Node* next;
+    Node() {}
+    Node(int _data, Node* next = nullptr) : data(_data), next(_next) {}
+  }
 };
 
 int main() { return 0; }
